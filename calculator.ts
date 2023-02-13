4c25a85b01f6534c3209ad8703dd16de8c0ad854
V A Afshan <123004009@sastra.ac.in>
2023-02-13 12:54:01 +0530

    new update

--- calculator.ts
+++ calculator.ts
@@ -1,18 +1,24 @@
+import morgan from 'morgan';
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan())
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-  let num1 = Number(req.body.n1);
-  let num2 = Number(req.body.n2);
-  let result = num1 + num2;
-  res.send("The result of calculation is : " + result);
+  const num1 = Number(req.body.n1);
+  const num2 = Number(req.body.n2);
+  const result = num1 + num2;
+  console.log(num1,num2,result)
+  res.send(`The sum of ${num1} and ${num2} is :   ${result}`);
 });
 
 app.listen(3000, function () {
